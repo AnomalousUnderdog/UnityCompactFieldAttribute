74c18d34934296e17ebc1a22dc60f2d2e6f6f4f5
Ferdinand Joseph Fernandez <ferdinandjosephfernandez@gmail.com>
2019-03-13 14:59:49 +0800

    Added files.

---
+++ CompactField/Editor/CompactFieldDrawer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(CompactFieldAttribute))]
+public class CompactFieldDrawer : PropertyDrawer
+{
+	Vector2 _propertyLabelSize;
+
+	const float INDENT_WIDTH = 16;
+
+	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+	{
+		return EditorGUI.GetPropertyHeight(property);
+	}
+
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		if (Event.current.type == EventType.Repaint)
+		{
+			_propertyLabelSize = EditorStyles.label.CalcSize(label);
+		}
+		
+		EditorGUIUtility.labelWidth = _propertyLabelSize.x + (EditorGUI.indentLevel * INDENT_WIDTH) + 5;
+		EditorGUI.PropertyField(position, property, label, true);
+		EditorGUIUtility.labelWidth = 0;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # UnityCompactFieldAttribute
 Lets each property label have their own width, to prevent the labels from getting cut off
+
+![Preview](/preview.gif "Preview")
+
+## Installation
+Copy the CompactField folder to your project.
+
+## Usage
+Add the CompactField attribute to your MonoBehaviour fields:
+
+```csharp
+		[CompactField]
+		public float testFloat = 3.0f;
+
+		[CompactField]
+		public bool testBool;
+```
